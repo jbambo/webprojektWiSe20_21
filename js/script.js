4536e825c906b34042b2e9c6ed7a2b78bc5e060b
jbambo <74155354+jbambo@users.noreply.github.com>
2020-11-30 20:10:10 +0100

    200iq move

--- js/script.js
+++ js/script.js
@@ -1,11 +1,44 @@
-console.log("hello world");
-
-function test(){
-    var x="hello";
-    console.log(x);
-}
-function toggleMobileNavigation(){
-   document.getElementById("mainNavi").classList.toggle("open");
+//toggle mobile navi
+function toggleMobileNavigation() {
+    document.getElementById("mainNavi").classList.toggle("open");
 }
 
+//event listener for mobile navi
 document.getElementById("toggle_menu").addEventListener("click", toggleMobileNavigation);
+
+//aside section
+
+//function for showing weekdays
+function showWeekdaysOfEmployee(element) {
+    let today = new Date(); //date object
+    let availableDays = [1, 2, 4, 5]; //array of available days 0-6
+    let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]//array of weekdays as strings
+    let todayWeekday = today.getDay(); //pass the day from object date to a variable
+    let availableWeekdays = []; //empty array
+
+    //for loop to iterate over available days
+    for (let i = 0; i < availableDays.length; i++) {
+        let weekdayText = weekdays[availableDays[i]]; //set weekdaytext to a available day
+
+        if (todayWeekday == availableDays[i]) { //if today is the available day make it span element
+            weekdayText = "<span>" + weekdayText + "</span>";
+        }//if
+        availableWeekdays.push(weekdayText); //fill the empty array with available days
+    }//end for loop
+
+    let sentenceAvailability = "I am available on the following days: <br>"+ availableWeekdays.join(", ");//print array to string with ", " between the elements
+
+    element.innerHTML= sentenceAvailability; //pass the computed string to element
+
+}// function end
+
+let availabilityContainer= document.getElementById("availability_days"); //pass the element with id to variable
+//check if the element in DOM
+if (availabilityContainer){
+    showWeekdaysOfEmployee(availabilityContainer); //initialize the function with variable
+}
+
+
+
+
+
