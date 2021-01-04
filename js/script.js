b4786b628d6d00b23f072a0ff441b9e2c364c755
jbambo <74155354+jbambo@users.noreply.github.com>
2021-01-04 19:44:26 +0100

    stronk

--- js/script.js
+++ js/script.js
@@ -26,15 +26,15 @@ function showWeekdaysOfEmployee(element) {
         availableWeekdays.push(weekdayText); //fill the empty array with available days
     }//end for loop
 
-    let sentenceAvailability = "I am available on the following days: <br>"+ availableWeekdays.join(", ");//print array to string with ", " between the elements
+    let sentenceAvailability = "I am available on the following days: <br>" + availableWeekdays.join(", ");//print array to string with ", " between the elements
 
-    element.innerHTML= sentenceAvailability; //pass the computed string to element
+    element.innerHTML = sentenceAvailability; //pass the computed string to element
 
 }// function end
 
-let availabilityContainer= document.getElementById("availability_days"); //pass the element with id to variable
+let availabilityContainer = document.getElementById("availability_days"); //pass the element with id to variable
 //check if the element is in DOM
-if (availabilityContainer){
+if (availabilityContainer) {
     showWeekdaysOfEmployee(availabilityContainer); //initialize the function with variable
 }
 
@@ -42,13 +42,95 @@ if (availabilityContainer){
 
 let radioButtons = document.getElementsByName("format");
 
-radioButtons.forEach(function(element){
-    element.addEventListener("click",function (){
+radioButtons.forEach(function (element) {
+    element.addEventListener("click", function () {
         let contactForm = document.getElementById("contact_form");
-        contactForm.classList.remove("newsletter","contact");
+        contactForm.classList.remove("newsletter", "contact");
         contactForm.classList.add(element.value)
     });
 });
+
+// validate form
+function checkContactForm() {
+
+    let format = document.getElementById("contact_form").classList.value;
+
+    let name = document.getElementsByName("username")[0];
+    let email = document.getElementsByName("usermail")[0];
+    let message = document.getElementsByName("user_message")[0];
+    let errorMessage = document.getElementById("error_messages");
+    let phoneNumber = document.getElementsByName("phone")[0];
+    let errorMessageText = "";
+
+
+    errorMessage.innerHTML = errorMessageText;
+
+
+    errorMessage.classList.remove("show");
+    name.parentElement.classList.remove("error");
+    email.parentElement.classList.remove("error");
+    message.parentElement.classList.remove("error");
+    phoneNumber.parentElement.classList.remove("error");
+
+   //check name
+    if (name.value === "" && format === "contact") {
+        name.parentElement.classList.add("error");
+        errorMessageText += "Enter name!<br>";
+    }
+
+    //check  email
+    if (email.value === "" || email.value.indexOf("@") === -1) {
+        email.parentElement.classList.add("error");
+        errorMessageText += "Enter valid email!<br>";
+    }
+
+    if (phoneNumber.value === "" && format === "contact") {
+        phoneNumber.parentElement.classList.add("error");
+        errorMessageText += "Enter phone number!<br>";
+    }
+
+    //check message
+    if (message.value === "" && format === "contact") {
+        message.parentElement.classList.add("error");
+        errorMessageText += "Enter message!<br>";
+    }
+
+
+    //show error text or show thanks
+    if (errorMessageText) {
+        errorMessage.innerHTML = errorMessageText;
+        errorMessage.classList.add("show");
+    } else {
+        location.href = "danke.html";
+    }
+
+    console.log(format);
+}
+
+
+document.getElementById("submitButton").addEventListener("click", checkContactForm);
+
+
+//back to top
+function scrollToTop() {
+    window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+}
+
+document.getElementById("scrollToTop").addEventListener("click", scrollToTop);
+
+document.body.onscroll = function () {
+    document.getElementById("scrollToTop").classList.remove("show");
+
+    if (self.pageYOffset > 300) {
+        document.getElementById("scrollToTop").classList.add("show");
+    }
+};
+
+
 
 
 
